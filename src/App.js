0efb060becab79b4bf39a579ac6b3de78e7836a7
krishna rokhale <krishna.rokhale@gmail.com>
2021-01-11 17:16:21 -0800

    checking in

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ import 'antd/dist/antd.css';
 import { DatePicker, List, Typography, Divider, Input, Popconfirm, message } from 'antd';
 import Calculator from "./calculator";
 
-
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-];
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useParams
+} from "react-router-dom";
 
 
 // Child components
@@ -102,6 +102,59 @@ const data = [
 //     }
 //
 // }
+
+function Home() {
+    return <div>
+        <h2>Home</h2>
+
+
+
+    </div>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Login() {
+    return <h2>Login Component</h2>;
+}
+
+function SomeComponent() {
+    // We can use the `useParams` hook here to access
+    // the dynamic pieces of the URL.
+    let { userId, profileId } = useParams();
+
+    // write some code here that fetches information about userId and grabs their profile using the profileId from the server
+
+    return (
+        <div>
+            {/*<h3>ID: {id}</h3>*/}
+            <h1>this is the SomeComponent</h1>
+
+            <h3>User ID: {userId}</h3>
+            <h3>Profile ID: {profileId}</h3>
+        </div>
+    );
+}
+
+function Users() {
+    return <div>
+
+        <Redirect to="/login" />
+
+        <h2>This is the users page</h2>
+
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <h2>This is the end of the users page</h2>
+
+    </div>;
+}
 
 function App() {
 
@@ -226,136 +279,56 @@ function App() {
     };
 
   return (
-    <div className="App" style={{border: '1px solid #ddd'}}>
-      {/*JSX*/}
-      <header className="App-header">
+      <Router>
+        <div className="App" style={{border: '1px solid #ddd'}}>
+          {/*JSX*/}
+          <header className="App-header">
 
-          <br/>
-          <br/>
-          <br/>
-
-
-          <List
-              size="large"
-              header={<div>
-                  <Input onPressEnter={(event) => {
-                      console.log(event.currentTarget.value);
-                      let txt = event.currentTarget.value;
-                      setTodos(oldArray => [...oldArray, txt]);
-
-                  }} />
-              </div>}
-              // footer={<div>Footer</div>}
-              bordered
-              // dataSource={data}
-              dataSource={todos}
-              renderItem={(item) => {
-                  return <List.Item>
-                      <span className={'text-gray-600'}>{item}</span>
-
-                      {/*<Popconfirm*/}
-                      {/*    title="Are you sure?"*/}
-                      {/*    onConfirm={() => {*/}
-                      {/*        // this is where you write code to delete the item*/}
-
-                      {/*    }}*/}
-                      {/*    // onCancel={cancel}*/}
-                      {/*    okText="Yes"*/}
-                      {/*    cancelText="No"*/}
-                      {/*>*/}
-                      {/*    <Button size={'small'} type={'danger'}>Delete</Button>*/}
-                      {/*</Popconfirm>*/}
-
-
-                  </List.Item>
-              }}
-          />
-
-          {JSON.stringify(todos)}
-
-
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-
-
-          {/*<div className={'p-10 bg-gray-200'}>*/}
-          {/*    yay*/}
-          {/*</div>*/}
+              <br/>
+              <br/>
+              <br/>
 
 
 
-          {/*<TopBar />*/}
+              <div>
+                  <nav>
+                      <ul>
+                          <li>
+                              <Link to="/">Home</Link>
+                          </li>
+                          <li>
+                              <Link to="/about">About</Link>
+                          </li>
+                          <li>
+                              <Link to="/users">Users</Link>
+                          </li>
+                      </ul>
+                  </nav>
 
-          {/*<MainContent />*/}
-
-          {/*<SideBar />*/}
-
-
-          {/*<div>top bar</div>*/}
-          {/*<div>main content</div>*/}
-          {/*<div>side bar</div>*/}
-
-          {/*<TopBar />*/}
-          {/*<MainContent />*/}
-          {/*<SideBar />*/}
-
-
-          <div>
-              <h1>Result is: {calculatorResult} </h1>
-          </div>
-
-      {/*    let func = (result) => {*/}
-      {/*    setCalculatorResult(result);*/}
-      {/*}*/}
-      {/*    let props = {onResultUpdate: func}*/}
-
-          <Calculator onResultUpdate={updateResult} />
-
-          {/*<Button buttonText={'Click Me'}*/}
-          {/*        size={'large'}*/}
-          {/*        someFunc={onSomeButtonClicked} />*/}
-
-          {/*<Button buttonText={'Submit'} size={'large'}/>*/}
-          {/*<Button buttonText={'do this'}/>*/}
-
-
-
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-
-          {/*{JSON.stringify(list)}*/}
-
-          <input type="text" onChange={(event) => {
-              // console.log(event.currentTarget.value);
-              // setTxt(event.currentTarget.value.toUpperCase());
-              // setTxt(event.currentTarget.value);
-              setCurrentItem(event.currentTarget.value)
-          }}/>
-          <br/>
-          <button onClick={addTheItem}>Add the Item</button>
-
-          <br/>
-          <br/>
-          <hr/>
-          <ul>
-              {list.map((listItem, idx) => {
-                  // return <li key={idx}>
-                  //     {listItem}
-                  // </li>
-
-                  return <ToDoListItem listItem={listItem} />
-              })}
-          </ul>
+                  {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                  <Switch>
+                      {/*<Route path="/about">*/}
+                      {/*    <About />*/}
+                      {/*</Route>*/}
+                      {/*<Route path="/about">*/}
+                      {/*    <Users />*/}
+                      {/*</Route>*/}
+                      {/*<Route path="/users">*/}
+                      {/*    <Users />*/}
+                      {/*</Route>*/}
+                      {/*<Route path="/">*/}
+                      {/*    <Home />*/}
+                      {/*</Route>*/}
+                      {/*<Route path="/login">*/}
+                      {/*    <Login />*/}
+                      {/*</Route>*/}
 
 
-          <hr/>
+                      <Route path="/users/:userId/profile/:profileId" children={<SomeComponent />} />
+
+                  </Switch>
+              </div>
 
 
 
@@ -365,69 +338,205 @@ function App() {
 
 
 
-          <div style={{textAlign: 'center'}}>
-              <h1>Timer: {secondsCounter}</h1>
-              {/*{secondsCounter==5 && <h2>The time now is 5 seconds!</h2>}*/}
-              {/*{(secondsCounter>5 && secondsCounter<11) && <h2>The seconds are between 5 and 10 seconds</h2>}*/}
 
-              {/*{(secondsCounter==5 || secondsCounter==8) && <h2>Only show up when the seconds counter is 5 OR 8</h2>}*/}
 
-              {/*{(secondsCounter!=5) && <h2>This will show up when seconds counter is not 5</h2>}*/}
 
-              {/*{console.log('was here')}*/}
-          </div>
-          <br/>
-          <div>
-              <h1>Names:</h1>
+              <List
+                  size="large"
+                  header={<div>
+                      <Input onPressEnter={(event) => {
+                          console.log(event.currentTarget.value);
+                          let txt = event.currentTarget.value;
+                          setTodos(oldArray => [...oldArray, txt]);
+
+                      }} />
+                  </div>}
+                  // footer={<div>Footer</div>}
+                  bordered
+                  // dataSource={data}
+                  dataSource={todos}
+                  renderItem={(item) => {
+                      return <List.Item>
+                          <span className={'text-gray-600'}>{item}</span>
+
+                          {/*<Popconfirm*/}
+                          {/*    title="Are you sure?"*/}
+                          {/*    onConfirm={() => {*/}
+                          {/*        // this is where you write code to delete the item*/}
+
+                          {/*    }}*/}
+                          {/*    // onCancel={cancel}*/}
+                          {/*    okText="Yes"*/}
+                          {/*    cancelText="No"*/}
+                          {/*>*/}
+                          {/*    <Button size={'small'} type={'danger'}>Delete</Button>*/}
+                          {/*</Popconfirm>*/}
+
+
+                      </List.Item>
+                  }}
+              />
+
+              {JSON.stringify(todos)}
+
+
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+
+
+              {/*<div className={'p-10 bg-gray-200'}>*/}
+              {/*    yay*/}
+              {/*</div>*/}
+
+
+
+              {/*<TopBar />*/}
+
+              {/*<MainContent />*/}
+
+              {/*<SideBar />*/}
+
+
+              {/*<div>top bar</div>*/}
+              {/*<div>main content</div>*/}
+              {/*<div>side bar</div>*/}
+
+              {/*<TopBar />*/}
+              {/*<MainContent />*/}
+              {/*<SideBar />*/}
+
+
+              <div>
+                  <h1>Result is: {calculatorResult} </h1>
+              </div>
+
+          {/*    let func = (result) => {*/}
+          {/*    setCalculatorResult(result);*/}
+          {/*}*/}
+          {/*    let props = {onResultUpdate: func}*/}
+
+              <Calculator onResultUpdate={updateResult} />
+
+              {/*<Button buttonText={'Click Me'}*/}
+              {/*        size={'large'}*/}
+              {/*        someFunc={onSomeButtonClicked} />*/}
+
+              {/*<Button buttonText={'Submit'} size={'large'}/>*/}
+              {/*<Button buttonText={'do this'}/>*/}
+
+
+
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+
+              {/*{JSON.stringify(list)}*/}
+
+              <input type="text" onChange={(event) => {
+                  // console.log(event.currentTarget.value);
+                  // setTxt(event.currentTarget.value.toUpperCase());
+                  // setTxt(event.currentTarget.value);
+                  setCurrentItem(event.currentTarget.value)
+              }}/>
+              <br/>
+              <button onClick={addTheItem}>Add the Item</button>
+
+              <br/>
+              <br/>
+              <hr/>
               <ul>
-                  {/*look up the .map and forEach in javascript and the differences between the two*/}
-                  {names.map((name) => {
-                      return <li key={name}>{name}</li>
+                  {list.map((listItem, idx) => {
+                      // return <li key={idx}>
+                      //     {listItem}
+                      // </li>
+
+                      return <ToDoListItem listItem={listItem} />
                   })}
-                  {/*<li>Two name</li>*/}
               </ul>
-          </div>
-          <br/>
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
 
-          <input type="text" onChange={(event) => {
-              console.log(event.currentTarget.value);
-              // setTxt(event.currentTarget.value.toUpperCase());
-              setTxt(event.currentTarget.value);
-          }}/>
-          <br/>
-          <button onClick={addTheName}>Add to names</button>
 
-          <div>
-              The value for the counter is: {counter}
-          </div>
-
-          <div>
-              {/*ternary operator*/}
-              <h1 style={txt=='john' ? {color: 'green'} : {color: 'red'}} className={txt=='john' ? 'bg-white' : 'bg-gray'}>The value for the txt is: {txt}</h1>
-              {/*{txt=='John' &&  <h1>It's John!</h1>}*/}
-          </div>
-
-          <br/>
-          <br/>
-          <button onClick={clickMe} id={'my-button'}>Click me</button>
-      </header>
+              <hr/>
 
 
 
-    </div>
+
+
+
+
+
+
+              <div style={{textAlign: 'center'}}>
+                  <h1>Timer: {secondsCounter}</h1>
+                  {/*{secondsCounter==5 && <h2>The time now is 5 seconds!</h2>}*/}
+                  {/*{(secondsCounter>5 && secondsCounter<11) && <h2>The seconds are between 5 and 10 seconds</h2>}*/}
+
+                  {/*{(secondsCounter==5 || secondsCounter==8) && <h2>Only show up when the seconds counter is 5 OR 8</h2>}*/}
+
+                  {/*{(secondsCounter!=5) && <h2>This will show up when seconds counter is not 5</h2>}*/}
+
+                  {/*{console.log('was here')}*/}
+              </div>
+              <br/>
+              <div>
+                  <h1>Names:</h1>
+                  <ul>
+                      {/*look up the .map and forEach in javascript and the differences between the two*/}
+                      {names.map((name) => {
+                          return <li key={name}>{name}</li>
+                      })}
+                      {/*<li>Two name</li>*/}
+                  </ul>
+              </div>
+              <br/>
+            {/*<img src={logo} className="App-logo" alt="logo" />*/}
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+
+              <input type="text" onChange={(event) => {
+                  console.log(event.currentTarget.value);
+                  // setTxt(event.currentTarget.value.toUpperCase());
+                  setTxt(event.currentTarget.value);
+              }}/>
+              <br/>
+              <button onClick={addTheName}>Add to names</button>
+
+              <div>
+                  The value for the counter is: {counter}
+              </div>
+
+              <div>
+                  {/*ternary operator*/}
+                  <h1 style={txt=='john' ? {color: 'green'} : {color: 'red'}} className={txt=='john' ? 'bg-white' : 'bg-gray'}>The value for the txt is: {txt}</h1>
+                  {/*{txt=='John' &&  <h1>It's John!</h1>}*/}
+              </div>
+
+              <br/>
+              <br/>
+              <button onClick={clickMe} id={'my-button'}>Click me</button>
+          </header>
+
+
+
+        </div>
+      </Router>
   );
 }
+
+
 
 export default App;
